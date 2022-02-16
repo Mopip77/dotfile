51bc8090833e4b54b68a75b325b208c0070a14a6
mopip77 <mopip77@qq.com>
2022-02-16 10:35:21 +0800

    yabai: fix changing wallpaper among multiple displays

--- tag-yabai/config/yabai/change-random-wallpaper.sh
+++ tag-yabai/config/yabai/change-random-wallpaper.sh
@@ -7,5 +7,15 @@ then
   # 分批打乱两次
   wallpaper=$(ls $RANDOM_FOLDER | shuf -n 5 | shuf -n 1)
   echo "change new wallpaper ${wallpaper}"
-  osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'${RANDOM_FOLDER}/${wallpaper}'"'
+  #osascript -e 'tell application "Finder" to set desktop picture to POSIX file "'${RANDOM_FOLDER}/${wallpaper}'"'
+  #osascript -e 'tell application "current desktop" to set picture to POSIX file "'${RANDOM_FOLDER}/${wallpaper}'"'
+
+  osascript -e "
+    tell application \"System Events\"
+      tell current desktop
+        set picture to \"${RANDOM_FOLDER}/${wallpaper}\"
+      end tell
+    end tell
+  "
+
 fi
