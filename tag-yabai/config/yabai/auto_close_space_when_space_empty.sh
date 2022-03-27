d0feb2111e7456f0d1d98ad01a322d57aa390af9
mopip77 <mopip77@qq.com>
2022-03-27 13:10:08 +0800

    yabai: fix destroy space bug

--- tag-yabai/config/yabai/auto_close_space_when_space_empty.sh
+++ tag-yabai/config/yabai/auto_close_space_when_space_empty.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-destroyed_window_id=$1
+set -x
 
-space_index=$(yabai -m query --windows --window $destroyed_window_id | jq '.space')
+echo "--------------deub---------------"
+
+space_index=$(yabai -m query --spaces --space | jq '.index')
 
 if [[ -n $space_index && "0" != $space_index ]]; then
   active_window_count=$(yabai -m query --windows --space $space_index | jq 'map(select(."is-sticky" == false)) | length')
   if [[ -n $active_window_count && "0" = $active_window_count ]]; then
     ~/.config/yabai/destroy_space.sh
+  fi
 fi
 
