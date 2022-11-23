bbac4fb1586828d1bffee3a61399c20ccb83dd85
mopip77 <mopip77@qq.com>
2022-11-23 13:26:24 +0800

    yabai: alt - o toggle pip

---
+++ tag-yabai/config/yabai/toggle-window-sticking-tiling.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Toggle a window between floating and tiling.
+# Only works when the workspace layout is bsp, i.e., the windows in it are tiled.
+
+spaceType=$(yabai -m query --spaces --space | jq .type)
+if [ $spaceType = '"bsp"' ]; then
+
+  read -r id sticky <<< $(echo $(yabai -m query --windows --window | jq '.id, ."is-sticky"'))
+  tmpfile=/tmp/yabai-tiling-floating-toggle/sticky-$id
+
+  # border=$(yabai -m config window_border)
+
+  # If the window is floating, record its position and size into a temp file and toggle it to be tiling.
+  if [[ $sticky = true ]]
+  then
+    [ -e $tmpfile ] && rm $tmpfile
+    echo $(yabai -m query --windows --window | jq .frame) >> $tmpfile
+    yabai -m window --toggle sticky \
+    && yabai -m window --toggle topmost \
+    && ~/.config/yabai/change-window-border-color-when-window-focused.sh
+  else
+    yabai -m window --toggle sticky \
+    && yabai -m window --toggle topmost \
+    && ~/.config/yabai/change-window-border-color-when-window-focused.sh
+    if [ -e $tmpfile ]
+    then
+      read -r x y w h <<< $(echo $(cat $tmpfile | jq '.x, .y, .w, .h'))
+      yabai -m window --move abs:$x:$y
+      yabai -m window --resize abs:$w:$h
+      rm $tmpfile
+    else
+      display=$(yabai -m query --windows --window | jq .display)
+      . /tmp/yabai-tiling-floating-toggle/display-$display
+       #move to top right corner
+      yabai -m window --move abs:$((w*4/5)):$((h/23))
+      yabai -m window --resize abs:$((w*3/5)):$((h*3/5))
+    fi
+  fi
+
+fi
