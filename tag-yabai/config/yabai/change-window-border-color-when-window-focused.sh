23e1dfee1e11384b542766d1cab9bf2776624f81
mopip77 <mopip77@qq.com>
2022-11-25 18:21:16 +0800

    yabai: update script

--- tag-yabai/config/yabai/change-window-border-color-when-window-focused.sh
+++ tag-yabai/config/yabai/change-window-border-color-when-window-focused.sh
@@ -4,13 +4,13 @@ window_id="${YABAI_WINDOW_ID:-}"
 
 read -r subrole sticky floating <<< $(echo $(yabai -m query --windows --window $window_id | jq -r '.subrole, ."is-sticky", ."is-floating"'))
 
-if [ "AXSystemDialog" = "$subrole" ]; then
+if [[ "AXSystemDialog" = "$subrole" ]]; then
     exit 0
 fi
 
-if [ "true" = $floating ]; then
+if [[ "true" = $floating ]]; then
   yabai -m config active_window_border_color 0xfc26A69A
-elif [ "true" = $sticky ]; then
+elif [[ "true" = $sticky ]]; then
   yabai -m config active_window_border_color 0xfcDD87AE
 else
   yabai -m config active_window_border_color 0xfcFFEE58
