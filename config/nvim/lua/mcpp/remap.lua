13eaf02f5c2df08633ecc81f249d87b3de422e90
mopip77 <mopip77@qq.com>
2023-01-10 01:29:15 +0800

    vim: remap

--- config/nvim/lua/mcpp/remap.lua
+++ config/nvim/lua/mcpp/remap.lua
@@ -1,7 +1,7 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- quick save and close
-vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>s", vim.cmd.w)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
 vim.keymap.set("n", "<leader>Q", "<cmd>q!<CR>")
 
@@ -10,6 +10,19 @@ vim.keymap.set({"n", "v"}, "<C-h>", "<C-w>h")
 vim.keymap.set({"n", "v"}, "<C-j>", "<C-w>j")
 vim.keymap.set({"n", "v"}, "<C-k>", "<C-w>k")
 vim.keymap.set({"n", "v"}, "<C-l>", "<C-w>l")
+
+-- buffer
+vim.keymap.set("n", "<leader>w", vim.cmd.bdelete)
+vim.keymap.set("n", "<tab>", vim.cmd.bn)
+vim.keymap.set("n", "<S-tab>", vim.cmd.bp)
+vim.keymap.set("n", "<leader>1", "<cmd>b1<CR>")
+vim.keymap.set("n", "<leader>2", "<cmd>b2<CR>")
+vim.keymap.set("n", "<leader>3", "<cmd>b3<CR>")
+vim.keymap.set("n", "<leader>4", "<cmd>b4<CR>")
+vim.keymap.set("n", "<leader>5", "<cmd>b5<CR>")
+vim.keymap.set("n", "<leader>6", "<cmd>b6<CR>")
+vim.keymap.set("n", "<leader>7", "<cmd>b7<CR>")
+vim.keymap.set("n", "<leader>8", "<cmd>b8<CR>")
 
 -- visual模式上下移动选中块
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -39,7 +52,23 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 
 -- 快速替换当前word的snippet
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>c", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- 快速增加可执行权限
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- 在visual模式下使用leader /? 快速查找选中内容
+vim.api.nvim_exec(
+  [[
+  function! g:VSetSearch(cmdtype)
+    let temp = @s
+    norm! gv"sy
+    let @/ = '\V' . substitute(escape(@s, a:cmdtype.'\'), '\n', '\\n', 'g')
+    let @s = temp
+  endfunction
+
+  xnoremap <leader>/ :<C-u>call g:VSetSearch('/')<CR>/<C-R>=@/<CR><CR>
+  xnoremap <leader>? :<C-u>call g:VSetSearch('?')<CR>?<C-R>=@/<CR><CR>
+]],
+  false
+)
