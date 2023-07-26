402ac397e3b696569d2e2af42440decf8506f986
mopip77 <mopip77@qq.com>
2023-07-26 13:51:34 +0800

    vim: remove copilot plugin

--- config/nvim/lua/mcpp/packer.lua
+++ config/nvim/lua/mcpp/packer.lua
@@ -54,7 +54,6 @@ return require('packer').startup(function(use)
   use('rcarriga/nvim-notify')
   -- terminal
   use("akinsho/toggleterm.nvim")
-  use("github/copilot.vim")
 
   -------------------- theme ----------------------------------
   use('shaunsingh/nord.nvim')
