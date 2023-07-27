7eac509eb3cbfd2479b7ae739de37be43ff4984a
mopip77 <mopip77@qq.com>
2023-07-27 09:41:35 +0800

    yabai: update some conf

--- config/sketchybar/items/now_playing.sh
+++ config/sketchybar/items/now_playing.sh
@@ -3,7 +3,7 @@
 sketchybar --add item now_playing center                              \
            --set now_playing     update_freq=3                        \
                                  script="$PLUGIN_DIR/now_playing.sh"   \
-                                 background.drawing=on        \
+                                 background.drawing=off        \
                                  background.color="$BACKGROUND"        \
                                  background.corner_radius=5            \
 
--- config/sketchybar/plugins/now_playing.sh
+++ config/sketchybar/plugins/now_playing.sh
@@ -4,7 +4,9 @@ IFS=$'\n' read -r -d$'\1' isPlaying title artist <<< "$(nowplaying-cli get playb
 
 if [[ $isPlaying != "1" ]]; then
     sketchybar --set $NAME label="" icon=""
+    sketchybar --set $NAME background.drawing=off
 else
-    sketchybar --set $NAME label="$title - $artist" icon="📻"
+    sketchybar --set $NAME label="$title - $artist" icon="🎹"
+    sketchybar --set $NAME background.drawing=on
 fi
 
