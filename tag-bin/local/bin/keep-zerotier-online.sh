029f8cebbd8b919cea1a060a498f93fc05894b92
mopip77 <mopip77@qq.com>
2023-08-01 21:57:08 +0800

    bin: add zt online

---
+++ tag-bin/local/bin/keep-zerotier-online.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+help() {
+    echo "Usage: $0 <ping-ip> <zt-network-id>"
+    echo ""
+    echo "Ping <ping-ip> first. If failed, leave and re-join <zt-network-id>"
+}
+
+main() {
+    if ! ping -c 1 -t 2 $1 &> /dev/null; then
+        echo "failed to ping $1, re-join $2"
+        sudo zerotier-cli leave $2
+        sudo zerotier-cli join $2
+    fi
+}
+
+if [[ $# -ne 2 ]]; then
+    help
+else
+    main $@
+fi
