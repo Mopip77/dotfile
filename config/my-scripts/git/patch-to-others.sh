37740a773b3dfa2d135cf79df9c34ae3088e9e0a
mopip77 <mopip77@qq.com>
2024-02-22 11:38:32 +0800

    myScript: git plugin

--- config/my-scripts/git/patch-to-others.sh
+++ config/my-scripts/git/patch-to-others.sh
@@ -2,13 +2,13 @@
 
 # patch one file diff to others
 if [[  $# -eq 0 || $1 = "-h" ]]; then
-  echo "./$0 <src> <dst>[...<dst>]"
+  echo "./$0 <diff-target-ref> <src> <dst>[...<dst>]"
 fi
 
 diff_file=$(mktemp)
-git diff $1 > $diff_file
+git diff $1 $2 > $diff_file
 
-for arg in "${@:2}"
+for arg in "${@:3}"
 do
   patch -p1 $arg $diff_file
 done
