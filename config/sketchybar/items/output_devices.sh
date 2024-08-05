7d25e49c43ed6f130004cd5128a31a4c554ed98a
mopip77 <mopip77@qq.com>
2024-08-05 20:52:38 +0800

    yabai: clickable output device area

--- config/sketchybar/items/output_devices.sh
+++ config/sketchybar/items/output_devices.sh
@@ -1,8 +1,12 @@
 #/bin/bash
 
 sketchybar --add item output_devices right                             \
-           --set output_devices      update_freq=5                    \
+           --set output_devices     \
+                         update_freq=5                    \
                          script="$PLUGIN_DIR/output_devices.sh"        \
                          background.color="$BACKGROUND"        \
                          background.corner_radius=5            \
-                         icon.drawing=off
+                         icon.drawing=off                      \
+                         click_script="open raycast://extensions/benvp/audio-device/set-output-device"
+
+
